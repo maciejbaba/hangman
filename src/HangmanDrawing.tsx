a115e954123a9e29f4a4515e0235adbc9bdd95a2
maciek5000 <maciek2baba2@gmail.com>
2022-11-06 16:33:45 +0100

    create gallow and the head

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,3 +1,46 @@
+const HEAD = (
+  <div style={{
+    height: "50px",
+    width: "50px",
+    borderRadius: "100%",
+    border: "10px solid black",
+    position: "absolute",
+    top: "50px",
+    right: "-30px"
+  }} />
+)
+
 export function HangmanDrawing() {
-  
+  return <div style={{ position: "relative" }}>
+    {HEAD}
+    <div style={{
+        position: "absolute",
+        height: "50px",
+        width: "10px",
+        background: "black",
+        top: 0,
+        right: 0
+      }}
+    />
+    <div style={{
+        height: "10px",
+        width: "200px",
+        background: "black",
+        marginLeft: "120px"
+      }}
+    />
+    <div style={{
+        height: "400px",
+        width: "10px",
+        background: "black",
+        marginLeft: "120px"
+      }}
+    />
+    <div style={{
+        height: "10px",
+        width: "250px",
+        background: "black"
+      }}
+    />
+  </div>
 }
