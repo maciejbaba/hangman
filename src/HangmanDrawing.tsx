18186d883b01b10e93fac320248e0185598ada4d
maciek5000 <maciek2baba2@gmail.com>
2022-11-06 18:48:28 +0100

    create whole body for the hangman

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -10,9 +10,77 @@ const HEAD = (
   }} />
 )
 
+const BODY = (
+  <div style={{
+    width: "10px",
+    height: "100px",
+    background: "black",
+    position: "absolute",
+    top: "120px",
+    right: 0
+  }} />
+)
+
+const RIGHT_ARM = (
+  <div style={{
+    width: "100px",
+    height: "10px",
+    background: "black",
+    position: "absolute",
+    top: "150px",
+    right: "-100px",
+    rotate: "-30deg",
+    transformOrigin: "left bottom"
+  }} />
+)
+
+const lEFT_ARM = (
+  <div style={{
+    width: "100px",
+    height: "10px",
+    background: "black",
+    position: "absolute",
+    top: "150px",
+    right: "10px",
+    rotate: "30deg",
+    transformOrigin: "right bottom"
+  }} />
+)
+
+const lEFT_LEG = (
+  <div style={{
+    width: "100px",
+    height: "10px",
+    background: "black",
+    position: "absolute",
+    top: "210px",
+    right: 0,
+    rotate: "-60deg",
+    transformOrigin: "right bottom"
+  }} />
+)
+
+const RIGHT_LEG = (
+  <div style={{
+    width: "100px",
+    height: "10px",
+    background: "black",
+    position: "absolute",
+    top: "210px",
+    right: "-90px",
+    rotate: "60deg",
+    transformOrigin: "left bottom"
+  }} />
+)
+
 export function HangmanDrawing() {
   return <div style={{ position: "relative" }}>
     {HEAD}
+    {BODY}
+    {RIGHT_ARM}
+    {lEFT_ARM}
+    {lEFT_LEG}
+    {RIGHT_LEG}
     <div style={{
         position: "absolute",
         height: "50px",
