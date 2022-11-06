b6618ed67756e4ba3f2b971b0c6c59a9d430c799
maciek5000 <maciek2baba2@gmail.com>
2022-11-06 20:43:57 +0100

    set up file for key styles and import it to keyboard component, set up keyboard component basic layout

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,3 +1,17 @@
+import styles from "./Keyboard.module.css"
+
+const STR_KEYS = "qwertyuiopasdfghjklzxcvbnm"
+const KEYS: string[] = STR_KEYS.split("")
+
 export function Keyboard() {
-  
+  return <div style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem"
+      }}
+    >
+    {KEYS.map(key => {
+      return <button key={key}>{key}</button>
+    })}
+    </div>
 }
